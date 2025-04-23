4f91ea3252b7ac4f4aa7449de6899346d5bbbb1f
Ivan <pavivand@gmail.com>
2025-04-24 02:25:37 +0300

    init

---
+++ create.sql
@@ -0,0 +1,24 @@
+create table users
+(
+    id       serial primary key,
+    username text,
+    password text
+);
+
+create table movie
+(
+    id                   serial primary key,
+    owner_id             int references users (id),
+    name                 text not null,
+    cord_x               int  not null,
+    cord_y               int  not null,
+    creation_date        timestamp default now(),
+    oscars_count         int check (oscars_count > 0),
+    total_box_office     int check (total_box_office > 0),
+    tagline              text,
+    genre                text not null,
+    director_name        text not null,
+    director_birthday    time,
+    director_weight      int,
+    director_nationality text not null
+);
