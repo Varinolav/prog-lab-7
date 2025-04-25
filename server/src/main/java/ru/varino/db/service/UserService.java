c6e45fe9883b70aeadd9f0cbad7aa26af2690795
Varinolav <pavivand@gmail.com>
2025-04-25 23:05:32 +0300

    123

--- server/src/main/java/ru/varino/db/service/UserService.java
+++ server/src/main/java/ru/varino/db/service/UserService.java
@@ -1,6 +1,35 @@
 package ru.varino.db.service;
 
+import ru.varino.common.models.User;
 import ru.varino.db.repository.UserRepository;
 
+import java.sql.Connection;
+import java.util.Optional;
+
 public class UserService implements UserRepository {
+    private Connection connection;
+
+    public UserService(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public Optional<User> findById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean checkPassword(User user, String password) {
+        return false;
+    }
 }
